79bbb09c8bca98b45dc1a68bd7e220292e04be7d
limon-chandra-ray <limonroy.19cse013@gmail.com>
2025-01-13 11:55:45 +0600

    'update-this=again'

--- src/component/page/Home/Home.jsx
+++ src/component/page/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home=()=>{
     return<>
-    <h1 className="min-h-screen bg-cyan-300">home</h1>
-    <h1>home</h1>
+        <h1 className="min-h-screen">home</h1>
+    
     </>
 }
 export default Home;
