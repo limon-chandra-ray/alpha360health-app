d6f86065b2cf4b9383dae86519a6547352bdfc7d
limon-chandra-ray <limonroy.19cse013@gmail.com>
2025-01-13 12:12:45 +0600

    'update-this-againg'

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Adjust if deploying to a subdirectory
+  build: {
+    outDir: 'dist', // Output directory for production
+  },
 })
